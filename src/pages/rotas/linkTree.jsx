9ca7464b1dec88caea4ca71d58dfb9ff27738530
vitorreisler <vitorreisler@gmail.com>
2025-12-11 11:38:20 -0300

    mudanca link tree

--- src/pages/rotas/linkTree.jsx
+++ src/pages/rotas/linkTree.jsx
@@ -2,17 +2,28 @@ import Image from "next/image";
 
 const LinkTree = () => {
     return ( 
-        <section className="flex flex-col items-center gap-3 mt-5">
+        <section className="flex flex-col items-center gap-5 mt-5">
             <h1 className="story-script-regular text-4xl text-center">Conheça minhas Redes Sociais</h1>
-                <a className="hover:scale-110 duration-300" href="https://youtube.com/channel/UCg7JpGJz9oINQJQZRNkGHBQ/" target="_blank" rel="noopener noreferrer">
-                    <Image src="/images/youtube-logo.webp" alt="Logo youtube" width={60} height={60} />
-                    </a>
-                <a className="hover:scale-110 duration-300"  href="https://www.instagram.com/silviamonteiro.art/" target="_blank" rel="noopener noreferrer">
-                    <Image className="rounded-xl" src="/images/instagram-logo.webp" alt="Logo instagram" width={60} height={60} />
-                    </a>
-                <a className="hover:scale-110 duration-300"  href="https://www.tiktok.com/@silviamonteiro.art?_r=1&_t=ZS-928NgBSPtZQ" target="_blank" rel="noopener noreferrer">
-                    <Image className="rounded-xl" src="/images/tiktok-logo.webp" alt="Logo tiktok" width={60} height={60} />
-                    </a>
+                <div className="flex gap-8">
+                    <a className="hover:scale-110 duration-300" href="https://youtube.com/channel/UCg7JpGJz9oINQJQZRNkGHBQ/" target="_blank" rel="noopener noreferrer">
+                        <Image src="/images/youtube-logo.webp" alt="Logo youtube" width={60} height={60} />
+                        </a>
+                    <a className="hover:scale-110 duration-300"  href="https://www.instagram.com/silviamonteiro.art/" target="_blank" rel="noopener noreferrer">
+                        <Image className="rounded-xl" src="/images/instagram-logo.webp" alt="Logo instagram" width={60} height={60} />
+                        </a>
+                    <a className="hover:scale-110 duration-300"  href="https://www.tiktok.com/@silviamonteiro.art?_r=1&_t=ZS-928NgBSPtZQ" target="_blank" rel="noopener noreferrer">
+                        <Image className="rounded-xl" src="/images/tiktok-logo.webp" alt="Logo tiktok" width={60} height={60} />
+                        </a>
+                </div>
+                <div className="flex gap-8">
+                    <a className="bg-[#9a5833] border border-black p-4 rounded-xl text-center hover:drop-shadow-2xl hover:scale-105 duration-400" href="https://cursossilvia.vercel.app/rotas/cursos/cursoPedrasResina" target="_blank" rel="noopener noreferrer">
+                        <strong className="text-white">
+                            Curso de velas com pedras e resina. 
+                        </strong>
+                        </a>
+                    
+                </div>
+            
         </section>
      );
 }
