7687206efec5944349f1971d18e6798f74daf52e
vitorreisler <vitorreisler@gmail.com>
2026-03-30 08:13:37 -0300

    Habilitei carrosel frutas e a pagina de curso

--- src/data/carroselData.js
+++ src/data/carroselData.js
@@ -2,16 +2,16 @@ export const carroselData = [
   
   {
     id: "1",
+    srcImg: "/images/capaFrutas.webp",
+    textoDentro: "Curso de Saboaria - Pintura de Frutas",
+    redirecionaPara:"/rotas/cursos/cursoPinturaSabonete",
+  },
+  {
+    id: "2",
     srcImg: "/images/curso-pedras-media.webp",
     textoDentro: "Velas com pedras e resina",
     redirecionaPara:"/rotas/cursos/cursoPedrasResina",
   },
-  //{
-   // id: "2",
-    //srcImg: "/images/capaFrutas.webp",
-    //textoDentro: "Curso de Saboaria - Pintura de Frutas",
-    //redirecionaPara:"/rotas/cursos/cursoPinturaSabonete",
- // },
  
 
 ];
